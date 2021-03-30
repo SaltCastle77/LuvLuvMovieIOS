30fb26d166bf12486f4fa402ceef2b26173966c3
lovelove33j1 <lovelove33j1@naver.com>
2021-03-30 23:11:25 +0900

    initial commit

---
+++ LuvLuvMovieIOS/ListViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ListViewController.swift
+//  LuvLuvMovieIOS
+//
+//  Created by 염성훈 on 2021/03/30.
+//
+
+import UIKit
+
+class ListViewController: UITableViewController{
+    
+}
---
+++ LuvLuvMovieIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LuvLuvMovieIOS
+//
+//  Created by 염성훈 on 2021/03/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
+
